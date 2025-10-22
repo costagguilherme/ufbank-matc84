e0522fbf76ffeee5da7d14a80a790040c56281d8
Guilherme Costa <costa.gguilherme02@gmail.com>
2025-10-22 13:51:18 -0300

    criando component button

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,17 @@
+interface ButtonProps {
+  type?: "button";
+  label: string;
+  onClick?: () => void;
+}
+
+export default function Button({ type = "button", label, onClick }: ButtonProps) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className="px-4 py-2 bg-blue-400 text-white rounded hover:bg-blue-500 transition-colors"
+    >
+      {label}
+    </button>
+  );
+}
