b1b4c86e40d7d5dd74c616118190737f450d6d4b
Guilherme Costa <costa.gguilherme02@gmail.com>
2025-10-23 16:51:55 -0300

    adicionando use client

--- src/components/templates/LoginTemplate.tsx
+++ src/components/templates/LoginTemplate.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import LoginForm from "../organism/LoginForm";
 
 export function LoginTemplate() {
--- src/components/templates/RegistrationTemplate.tsx
+++ src/components/templates/RegistrationTemplate.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import RegistrationForm from "../organism/RegistrationForm";
 
 export function RegistrationTemplate() {
