5d38433d8e55c8cee9b6d1a071bcbed9c5085ae9
Arnaldo Liro <dinholiro@gmail.com>
2025-10-23 12:56:05 -0300

    fix: placing the registration form on the main page and centering the div :p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,30 @@ import { useState } from "react";
 import Button from "../components/atoms/Button";
 import Input from "../components/atoms/Input";
 import FormCheckbox from "../components/molecules/FormCheckbox";
+import Label from "../components/atoms/Label";
+import RegistrationForm from "../components/organism/RegistrationForm";
 
 export default function Home() {
-  const [text, setText] = useState("");
-  const [aceite, setAceite] = useState(false);
+  // const [text, setText] = useState("");
+  // const [aceite, setAceite] = useState(false);
 
-  const getClick = () => {
-    alert(`texto: ${text} | termos: ${aceite}`);
-  };
+  // const getClick = () => {
+  //   alert(`texto: ${text} | termos: ${aceite}`);
+  // };
 
   return (
-    <div className="flex justify-center mt-12">
-      <label className="block text-sm font-medium text-gray-800">
-          Digite algo
-          <div className="mt-1">
-            <Input value={text} onChange={setText} placeholder="Digite algo aqq" />
-          </div>
-        </label>
-
-      <FormCheckbox
-        label="Aceito os termos de uso"
-        checked={aceite}
-        onChange={setAceite}
-        required
-      />
-      
-      <Button type="button" label="Clique aqui" onClick={getClick} />
-
+    // <div className="flex justify-center mt-12">
+    //   <label className="block text-sm font-medium text-gray-800">
+    //       Digite algo
+    //       <div className="mt-1">
+    //         <Input value={text} onChange={setText} placeholder="Digite algo aqq" />
+    //       </div>
+    //     </label>
+    // </div>
+    
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+      <RegistrationForm />
     </div>
+
   );
 }
