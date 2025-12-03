4df9abb7e9e93b932ab5d0f0e3cc8143f5b262bb
Guilherme Costa <costa.gguilherme02@gmail.com>
2025-12-03 14:46:18 -0300

    POST criacao de maquina de pagamento

--- src/app/api/machines/route.ts
+++ src/app/api/machines/route.ts
@@ -1,6 +1,20 @@
 import { machinesDB } from "../db";
 
-
 export async function GET() {
   return Response.json(machinesDB);
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const machine = {
+    id: machinesDB.length + 1,
+    name: body.name,
+    serial: body.serial,
+    model: body.model,
+  };
+
+  machinesDB.push(machine);
+
+  return Response.json(machine, { status: 200 });
 }
