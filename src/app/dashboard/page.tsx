49b96356facd31e4286e47944e02ced220ee93e3
Arnaldo Liro <dinholiro@gmail.com>
2025-12-18 14:08:27 -0300

    feature: Adding a button to log out of the session.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import KeyMetrics from "src/components/organism/KeyMetrics";
+import { signOut } from "next-auth/react";
+import Button from "@/src/components/atoms/Button";
 
 export default function Dashboard() {
   const { data: session, status } = useSession();
@@ -39,7 +41,14 @@ export default function Dashboard() {
   return (
     <div className="min-h-screen bg-gray-50 p-8">
       <div className="w-full max-w-7xl mx-auto">
-        <h1 className="text-3xl font-bold text-gray-900 mb-6">Painel de Controle</h1>
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-3xl font-bold text-gray-900 mb-6">Painel de Controle</h1>
+        <Button
+            type="button"
+            label="Sair"
+            onClick={() => signOut({ callbackUrl: "/login" })}
+          />
+        </div>
         <KeyMetrics machines={machines} />
         <div className="mt-8 text-center">
             <Link href="/machines" className="px-6 py-3 bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-1 text-lg">
--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -9,7 +9,7 @@ export default function Button({ type = "submit", label, onClick }: ButtonProps)
     <button
       type={type}
       onClick={onClick}
-      className="px-6 py-3 bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-1"
+      className="px-6 py-3 bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-1 cursor-pointer"
     >
       {label}
     </button>
